ef46fdfbe537afd653aaf0706c1389969c0c9187
Takahiro Mitsuoka <tmitsuoka0423@gmail.com>
2022-06-21 00:59:51 +0900

    add barcode

--- routes/api.php
+++ routes/api.php
@@ -2,10 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 use LINE\LINEBot;
 use LINE\LINEBot\Constant\HTTPHeader;
 use LINE\LINEBot\Event\MessageEvent\TextMessage;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use Picqer\Barcode\BarcodeGeneratorPNG;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,7 +28,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 $httpClient = new CurlHTTPClient($_ENV['LINE_CHANNEL_ACCESS_TOKEN']);
 $bot = new LINEBot($httpClient, ['channelSecret' => $_ENV['LINE_CHANNEL_SECRET']]);
 
-Route::post('/webhook', function (Request $request) use ($bot) {
+$barcodeGenerator = new BarcodeGeneratorPNG();
+
+Route::post('/webhook', function (Request $request) use ($bot, $barcodeGenerator) {
     Log::debug($request);
 
     $signature = $request->header(HTTPHeader::LINE_SIGNATURE);
@@ -36,9 +41,25 @@ Route::post('/webhook', function (Request $request) use ($bot) {
     $events = $bot->parseEventRequest($request->getContent(), $signature);
     Log::debug(['$events' => $events]);
 
-    collect($events)->each(function ($event) use ($bot) {
+    collect($events)->each(function ($event) use ($bot, $barcodeGenerator) {
         if ($event instanceof TextMessage) {
-            return $bot->replyText($event->getReplyToken(), $event->getText());
+            if ($event->getText() === '会員カード') {
+                // TODO: Airtableから`barcodeId`を取得
+                $barcodeId = $event->getUserId();
+                $barcodeFileName = "{$barcodeId}.png";
+                $barcodeFilePath = "./public/{$barcodeFileName}";
+
+                // バーコードが存在しなければ生成する
+                if (!Storage::exists($barcodeFilePath)) {
+                    $barcodeImage = $barcodeGenerator->getBarcode('barcodeId', $barcodeGenerator::TYPE_CODE_128);
+                    Storage::put($barcodeFilePath, $barcodeImage);
+                }
+
+                $imageMessageBuilder = new ImageMessageBuilder($barcodeFilePath, $barcodeFilePath);
+                $bot->replyMessage($event->getReplyToken(), $imageMessageBuilder);
+            } else {
+                $bot->replyText($event->getReplyToken(), $event->getText());
+            }
         }
     });
 
